e6d6a76f2e1c1fbd10bb4b85a2f3c859f9e7c3d2
LennyLesnikowski <98540779+LennyLesnikowski@users.noreply.github.com>
2022-02-24 14:17:18 -0800

    Add files via upload

---
+++ 2_1.py
@@ -0,0 +1,20 @@
+# speed = 50
+# time_elapsed = 14
+# distance = speed * time_elapsed
+# print (distance)
+
+# inches = 71
+# feet = inches // 12
+# bruh = inches % 12
+ ## print (feet, bruh)
+#bus = " sus "
+#mungus = " AHMUNGUUS"
+#bruh = bus + "&" + mungus
+#print(bruh)
+#length = len(bruh)
+#print(length)
+full_name = input ("Enter your full name:")
+ios = full_name .rfind(" ")
+print ("lastname:", full_name [ios+1:])
+print ("First Name (s):", full_name [:ios])
+
