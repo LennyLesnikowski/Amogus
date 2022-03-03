988c419fb79fd5fdf8923f197baddf5127f0cbe5
Lenny lesnikowski <lennylesnikowski@gmail.com>
2022-03-03 14:03:21 -0800

    crack

---
+++ 2_1.py
@@ -0,0 +1,33 @@
+# speed = 50
+# time_elapsed = 14
+# distance = speed * time_elapsed
+# print (distance)
+
+# inches = 71
+# feet = inches // 12
+# bruh = inches % 12
+ ## print (feet, bruh)
+#bus = " sus "
+#mungus = " AHMUNGUUS"
+#bruh = bus + "&" + mungus
+#print(bruh)
+#length = len(bruh)
+#print(length)
+# full_name = input ("Enter your full name:")
+# ios = full_name .rfind(" ")
+# print ("astname:", full_name [ios+1:])
+# print ("First Name (s):", full_name [:ios])
+## ctrl+/ to comment out section
+# annualIncome = eval(input ("enter your annual income"))
+# print ("your annual income is ", annualIncome)
+# monthlyIncome = annualIncome/12
+
+# print ("monthly income is ", monthlyIncome)
+var1 = 1
+var2 = 2
+var3 = "3"
+var4 = "4"
+var5 = var1+var2
+var6 = var3+var4 
+print (var5)
+print (var6)
